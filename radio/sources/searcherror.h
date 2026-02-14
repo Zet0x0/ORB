ef0b61e7265450273f245d90ab23e312077da1c8
Zet0x0 <58139125+Zet0x0@users.noreply.github.com>
2026-02-14 21:47:49 +0500

    add CONSTANT to title and message properties of SearchError

--- radio/sources/searcherror.h
+++ radio/sources/searcherror.h
@@ -6,8 +6,8 @@ struct SearchError {
     Q_GADGET
     QML_NAMED_ELEMENT(searchError)
 
-    Q_PROPERTY(QString title MEMBER title FINAL)
-    Q_PROPERTY(QString message MEMBER message FINAL)
+    Q_PROPERTY(QString title MEMBER title CONSTANT FINAL)
+    Q_PROPERTY(QString message MEMBER message CONSTANT FINAL)
 
 public:
     QString title;
