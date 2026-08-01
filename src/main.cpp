9b22e278a494b6e8723432fe5ecbca97bab52501
Zet <58139125+Zet0x0@users.noreply.github.com>
2026-08-01 18:00:07 +0500

    main.cpp: also set the palette for QGuiApplication
    because apparently some dipshits like Text with textFormat set to MarkdownText dont respect the palette set in ApplicationWindow (why?)
    
    so now we just set a palette twice...

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,62 @@
 #include "sources/sourcecontroller.h"
 #include <QGuiApplication>
 #include <QIcon>
+#include <QPalette>
 #include <QQmlApplicationEngine>
 #include <QQuickWindow>
+
+namespace {
+struct ColorRoleMapping {
+    QPalette::ColorRole role;
+    const char *propertyName;
+};
+
+constexpr ColorRoleMapping colorRoleMappings[] = {
+    {QPalette::Accent, "accent"},
+    {QPalette::AlternateBase, "alternateBase"},
+    {QPalette::Base, "base"},
+    {QPalette::BrightText, "brightText"},
+    {QPalette::Button, "button"},
+    {QPalette::ButtonText, "buttonText"},
+    {QPalette::Dark, "dark"},
+    {QPalette::Highlight, "highlight"},
+    {QPalette::HighlightedText, "highlightedText"},
+    {QPalette::Light, "light"},
+    {QPalette::Link, "link"},
+    {QPalette::LinkVisited, "linkVisited"},
+    {QPalette::Mid, "mid"},
+    {QPalette::Midlight, "midlight"},
+    {QPalette::PlaceholderText, "placeholderText"},
+    {QPalette::Shadow, "shadow"},
+    {QPalette::Text, "text"},
+    {QPalette::ToolTipBase, "toolTipBase"},
+    {QPalette::ToolTipText, "toolTipText"},
+    {QPalette::Window, "window"},
+    {QPalette::WindowText, "windowText"},
+};
+
+void applyColorGroup(QPalette &palette, QPalette::ColorGroup group,
+                     QObject *colorGroup) {
+    for (const ColorRoleMapping &mapping : colorRoleMappings) {
+        palette.setColor(
+            group, mapping.role,
+            colorGroup->property(mapping.propertyName).value<QColor>());
+    }
+}
+
+QPalette paletteFromQmlPalette(QObject *qmlPalette) {
+    QPalette palette;
+
+    applyColorGroup(palette, QPalette::Active,
+                    qmlPalette->property("active").value<QObject *>());
+    applyColorGroup(palette, QPalette::Inactive,
+                    qmlPalette->property("inactive").value<QObject *>());
+    applyColorGroup(palette, QPalette::Disabled,
+                    qmlPalette->property("disabled").value<QObject *>());
+
+    return palette;
+}
+}
 
 int main(int argc, char *argv[]) {
     QCoreApplication::setOrganizationName(QStringLiteral("zet0x0"));
@@ -25,6 +79,15 @@ int main(int argc, char *argv[]) {
         &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
         []() {
             QCoreApplication::exit(-1);
+        },
+        Qt::QueuedConnection);
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreated, &app,
+        [&app](QObject *object, const QUrl &) {
+            if (object) {
+                app.setPalette(paletteFromQmlPalette(
+                    object->property("palette").value<QObject *>()));
+            }
         },
         Qt::QueuedConnection);
 
