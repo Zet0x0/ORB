54fcd185253ae43a0de8f9dd472c715f182a65e2
Zet <58139125+Zet0x0@users.noreply.github.com>
2026-08-31 20:48:32 +0500

    keep the settings file where QSettings decides to put it
    usually %appdata%\zet0x0\orb.ini?

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <QQmlComponent>
 #include <QQuickStyle>
 #include <QQuickWindow>
+#include <QSettings>
 
 namespace {
 void applyColorGroup(QPalette &palette, QPalette::ColorGroup group,
@@ -52,6 +53,8 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setOrganizationDomain(QStringLiteral("com.zet0x0.orb"));
     QCoreApplication::setApplicationName(QStringLiteral("ORB"));
     QCoreApplication::setApplicationVersion(QStringLiteral("0.1.0"));
+
+    QSettings::setDefaultFormat(QSettings::IniFormat);
 
     QGuiApplication::setQuitOnLastWindowClosed(false);
 
